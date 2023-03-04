58c2c22c827349919ec4c92bcb7a12494d027f7c
waziri ally amiri <74810402+WAZIRI123@users.noreply.github.com>
2023-03-04 06:15:49 +0300

    change from server

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,49 +7,49 @@
        
         <title>{{ config('app.name', 'Grandez Booking System') }}</title>
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
+        
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
         <!-- Scripts -->
         @livewireStyles 
         @stack('jquery')
+
+
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    <style>
+        @stack('custom-style')
+     </style>
 </head>
-<body class="font-['Quicksand']">
+<body>
     <div class="relative overflow-x-hidden min-h-screen flex flex-col justify-between">
         <div class="relative pt-24 md:pt-32">
         <x-nav/>
        {{ $slot }}
         </div>
-        <footer class="py-8 border-t border-gray-200">
-            <div class="container px-10 mx-auto grid md:grid-cols-5 sm:grid-cols-3 grid-cols-2 gap-10">
-                <div class="space-y-2">
-                    <a href="{{ route('index') }}">
-                        <img src="{{ asset("img/brand/logo-1.png") }}" class="w-25 h-25 object-cover rounded-tr-xl rounded-bl-xl" alt="">
-                    </a>
-                </div>
-                <div class="space-y-2">
-                    <span class="uppercase tracking-widest text-gray-800 font-bold font-['poppins']">Contact Us</span>
+<footer class="py-8 border-t border-gray-200">
+  <div class="container px-4 md:px-10 mx-auto grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10 place-items-center items-start">
+    <div class="space-y-2">
+      <div class="map-section">
+        <span class="uppercase tracking-widest text-gray-800 font-bold font-['poppins']">Map</span>
+        <div class="aspect-w-16 aspect-h-9">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.529133368038!2d39.20946919689723!3d-6.193693555954718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x185cda657cac76bb%3A0xea7c784828bdee5!2sNyerere%20Rd%2C%20Zanzibar%2C%20Tanzania!5e0!3m2!1sen!2sus!4v1677671146328!5m2!1sen!2sus" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+      </div>
+    </div>
+
+    <div class="space-y-2">
+      <div class="Contact-us">
+                  <span class="uppercase tracking-widest text-gray-800 font-bold font-['poppins']">Contact Us</span>
                     <div class="flex flex-col space-y-2">
                         <a class="text-sm text-gray-600 hover:underline" href="#">+255719 280 165</a>
                         <a class="text-sm text-gray-600 hover:underline" href="#package">info@grandezzazanzibar.com</a>
                     </div>
-                </div>
-                <div class="space-y-2">
-                    <span class="uppercase tracking-widest text-gray-800 font-bold font-['poppins']">Quick Links</span>
-                    <div class="flex flex-col  space-y-2">
-                        <a class="text-sm text-gray-600 hover:underline" href="{{ route('activities.show') }}">Activies</a>
-                        <a class="text-sm text-gray-600 hover:underline" href="{{ route('cars.show') }}">Transfers & Car Rental</a>
-                        <a class="text-sm text-gray-600 hover:underline" href="{{ route('packagies.show') }}">Packagies</a>
-                    </div>
-                </div>
-                <div class="space-y-2">
-                    <span class="uppercase tracking-widest text-gray-800 font-bold font-['poppins']">Our Partner</span>
-                    <div class="flex flex-col space-y-2">
-                        <a class="text-sm text-gray-600 hover:underline" href="https://www.hyatt.com/en-US/hotel/tanzania/park-hyatt-zanzibar/znzph">Park Hyatt Zanzibar Hotel</a>
-                        <a class="text-sm text-gray-600 hover:underline" href="https://tembohotel.com/">Tembo Hotel</a>
-                        <a class="text-sm text-gray-600 hover:underline" href="https://www.zurihoteliloilo.com/">Zuri Hotel</a>
-                    </div>
-                </div>
+      </div>
+    </div>
+    
+
                 <div class="space-y-3">
                     <div class="uppercase dark:text-gray-50">Social media</div>
                     <div class="flex justify-start space-x-3">
@@ -68,17 +68,26 @@
                                 <path d="M16 0c-4.349 0-4.891 0.021-6.593 0.093-1.709 0.084-2.865 0.349-3.885 0.745-1.052 0.412-1.948 0.959-2.833 1.849-0.891 0.885-1.443 1.781-1.849 2.833-0.396 1.020-0.661 2.176-0.745 3.885-0.077 1.703-0.093 2.244-0.093 6.593s0.021 4.891 0.093 6.593c0.084 1.704 0.349 2.865 0.745 3.885 0.412 1.052 0.959 1.948 1.849 2.833 0.885 0.891 1.781 1.443 2.833 1.849 1.020 0.391 2.181 0.661 3.885 0.745 1.703 0.077 2.244 0.093 6.593 0.093s4.891-0.021 6.593-0.093c1.704-0.084 2.865-0.355 3.885-0.745 1.052-0.412 1.948-0.959 2.833-1.849 0.891-0.885 1.443-1.776 1.849-2.833 0.391-1.020 0.661-2.181 0.745-3.885 0.077-1.703 0.093-2.244 0.093-6.593s-0.021-4.891-0.093-6.593c-0.084-1.704-0.355-2.871-0.745-3.885-0.412-1.052-0.959-1.948-1.849-2.833-0.885-0.891-1.776-1.443-2.833-1.849-1.020-0.396-2.181-0.661-3.885-0.745-1.703-0.077-2.244-0.093-6.593-0.093zM16 2.88c4.271 0 4.781 0.021 6.469 0.093 1.557 0.073 2.405 0.333 2.968 0.553 0.751 0.291 1.276 0.635 1.844 1.197 0.557 0.557 0.901 1.088 1.192 1.839 0.22 0.563 0.48 1.411 0.553 2.968 0.072 1.688 0.093 2.199 0.093 6.469s-0.021 4.781-0.099 6.469c-0.084 1.557-0.344 2.405-0.563 2.968-0.303 0.751-0.641 1.276-1.199 1.844-0.563 0.557-1.099 0.901-1.844 1.192-0.556 0.22-1.416 0.48-2.979 0.553-1.697 0.072-2.197 0.093-6.479 0.093s-4.781-0.021-6.48-0.099c-1.557-0.084-2.416-0.344-2.979-0.563-0.76-0.303-1.281-0.641-1.839-1.199-0.563-0.563-0.921-1.099-1.197-1.844-0.224-0.556-0.48-1.416-0.563-2.979-0.057-1.677-0.084-2.197-0.084-6.459 0-4.26 0.027-4.781 0.084-6.479 0.083-1.563 0.339-2.421 0.563-2.979 0.276-0.761 0.635-1.281 1.197-1.844 0.557-0.557 1.079-0.917 1.839-1.199 0.563-0.219 1.401-0.479 2.964-0.557 1.697-0.061 2.197-0.083 6.473-0.083zM16 7.787c-4.541 0-8.213 3.677-8.213 8.213 0 4.541 3.677 8.213 8.213 8.213 4.541 0 8.213-3.677 8.213-8.213 0-4.541-3.677-8.213-8.213-8.213zM16 21.333c-2.948 0-5.333-2.385-5.333-5.333s2.385-5.333 5.333-5.333c2.948 0 5.333 2.385 5.333 5.333s-2.385 5.333-5.333 5.333zM26.464 7.459c0 1.063-0.865 1.921-1.923 1.921-1.063 0-1.921-0.859-1.921-1.921 0-1.057 0.864-1.917 1.921-1.917s1.923 0.86 1.923 1.917z"></path>
                             </svg>
                         </a>
+                                          <a rel="noopener noreferrer" href="https://www.tripadvisor.com/Attraction_Review-g8055401-d20176172-Reviews-Grandezza_Tours_Travel-Zanzibar_City_Zanzibar_Island_Zanzibar_Archipelago.html" title="TripAdvisor" class="flex items-center p-1">
+                    <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-7 w-7"
+    fill="currentColor"
+    style="color: #128c7e"
+    viewBox="0 0 24 24">
+    <path
+      d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z" />
+  </svg>
+                        </a>
+                        
                     </div>
                 </div>
             </div>
             <div class="py-6 text-sm text-center dark:text-gray-400">©  {{ date('Y') }} Grandezza . All rights reserved.</div>
         </footer>
+        <div class="elfsight-app-0de37a9f-4330-44ef-9ad9-4fa544e93df3"></div>
     </div>
-    
     <livewire:scripts />
-    <script>
-
-        @stack('scripts')
-    </script>
+    {{ $js ?? '' }}
 </body>
 </html>
